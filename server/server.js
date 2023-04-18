8f474e311ea40b2ba65ad5311ce5bf0618819079
BLystrup <brittany.lystrup@gmail.com>
2023-04-17 21:04:36 -0700

    Added backend login and reg

--- server/server.js
+++ server/server.js
@@ -2,11 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 8000;
+const cookieParser = require('cookie-parser');
+
+require('dotenv').config();
 
 require('./config/mongoose.config');
-app.use(cors());
+
+app.use(cookieParser());
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 app.use(express.json(), express.urlencoded({ extended: true }));
 
 require('./routes/garment.routes')(app);
+require('./routes/user.routes')(app);
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
